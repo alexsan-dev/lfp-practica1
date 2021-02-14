26c19dae1068ed628bce485cd53431f5c99d2650
Alex Santos <alexdsantosv@gmail.com>
2021-02-13 22:41:16 -0600

    [ Add ] style to html

--- files/html.py
+++ files/html.py
@@ -1,5 +1,7 @@
 def generate_html(content):
-    html = f'<!DOCTYPE html><html lang="es"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Document</title></head><body><p>{content}</p></body></html>'
+    html = '<!DOCTYPE html><html lang="es"> <head> <meta charset="UTF-8"/> <meta name="viewport" content="width=device-width, initial-scale=1.0"/> <link rel="preconnect" href="https://fonts.gstatic.com"/> <link href="https://fonts.googleapis.com/css2?family=Cabin&display=swap" rel="stylesheet"/> <title>Practica 01</title> <style>*{margin: 0; padding: 0; box-sizing: border-box; font-family: "Cabin", sans-serif;}body{background: #f5f5f5; display: flex; align-items: center; justify-content: center; padding: 30px; height: 100vh;}#container{background-color: #ffffff; width: 100%; max-width: 620px; border-radius: 10px; overflow: hidden; box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.3);}a{color: #fff; font-weight: bold;}#header{padding: 40px; background: #2196f3; color: #fff;}#card{background: #555; padding: 40px;}#card > p{font-family: monospace; font-size: 1.2em; color: #fff; font-weight: bold;}</style> </head> <body> <div id="container"> <div id="header"> <h1>Resultados de archivos</h1> <p id="description"></p></div><div id="card"> <p>' + \
+        content + \
+        '</p></div></div><script>const p=document.getElementById("description"); p.innerHTML=`Estos son los datos generados el ${new Date().toLocaleDateString( "en-GB" )}a las ${new Date().toLocaleTimeString( "en-US" )}por los archivos .lfp en la aplicación. ver app en <a target="_blank" href="https://github.com/alexsan-dev/lfp-practica1">https://github.com/alexsan-dev/lfp-practica1</a>`; </script> </body></html>'
     file = open('./index.html', "w")
-    file.write(html)
+    file.write(html.replace('\n', '<br/>'))
     file.close()
--- files/reader.py
+++ files/reader.py
@@ -4,14 +4,15 @@ from tkinter import *
 
 class lfp_reader():
     # SELECCIONAR ARCHIVO
-    # def __init__(self):
-    #     self.lfp_file = Tk()
-    #     self.lfp_file.filename = filedialog.askopenfilename(
-    #         initialdir="/", title="Seleccionar archivo", filetypes=(("LFP", "*.lfp"), ("all files", "*.*")))
+
+    def __init__(self):
+        self.lfp_file = Tk()
+        self.lfp_file.filename = filedialog.askopenfilename(
+            initialdir="/", title="Seleccionar archivo", filetypes=(("LFP", "*.lfp"), ("all files", "*.*")))
 
     # OBTENER
     def get_lines(self, keyword):
-        lfp_stream = open('./test.lfp')
+        lfp_stream = open(self.lfp_file.filename)
         lfp_lines = lfp_stream.readlines()
         selected_lines = [words for words in lfp_lines if keyword in words]
         lfp_stream.close()
