fa9fe8eabb745c4cb210bd17c99a1f0358e925e8
Alex Santos <alexdsantosv@gmail.com>
2021-02-13 23:01:04 -0600

    [ Add ] auto hide tkinter

--- files/reader.py
+++ files/reader.py
@@ -1,19 +1,25 @@
 from tkinter import filedialog
 from tkinter import *
 
+root = Tk()
+root.wm_withdraw()
+
 
 class lfp_reader():
     # SELECCIONAR ARCHIVO
 
     def __init__(self):
-        self.lfp_file = Tk()
-        self.lfp_file.filename = filedialog.askopenfilename(
+        self.filename = filedialog.askopenfilename(
             initialdir="/", title="Seleccionar archivo", filetypes=(("LFP", "*.lfp"), ("all files", "*.*")))
+        root.destroy()
 
     # OBTENER
     def get_lines(self, keyword):
-        lfp_stream = open(self.lfp_file.filename)
+        # LEER LINEAS
+        lfp_stream = open(self.filename)
         lfp_lines = lfp_stream.readlines()
         selected_lines = [words for words in lfp_lines if keyword in words]
+
+        # CERRAR STREAM
         lfp_stream.close()
         return selected_lines
